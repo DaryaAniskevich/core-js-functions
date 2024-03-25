474605233d5bc7ccd3240e5e5b39e561d73ba02f
DaryaAniskevich <darya.aniskevich@mail.ru>
2024-03-25 14:45:13 +0300

    feat: 2-5 desicions

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -32,8 +32,8 @@ function getCurrentFunctionName() {
  *   getFunctionBody(hiHello) => "function hiHello() { console.log('hello world'); }"
  *
  */
-function getFunctionBody(/* func */) {
-  throw new Error('Not implemented');
+function getFunctionBody(func) {
+  return func ? func.toString() : '';
 }
 
 /**
@@ -50,8 +50,8 @@ function getFunctionBody(/* func */) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  return funcs ? funcs.map((func) => func.length) : [];
 }
 
 /**
@@ -70,10 +70,9 @@ function getArgumentsCount(/* funcs */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return (x) => x ** exponent;
 }
-
 /**
  * Returns the polynom function of one argument based on specified coefficients.
  * See: https://en.wikipedia.org/wiki/Polynomial#Definition
@@ -87,8 +86,22 @@ function getPowerFunction(/* exponent */) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...args) {
+  if (args.length) {
+    if (args.length === 3) {
+      const [a, b, c] = args;
+      return (x) => a * x ** 2 + b * x + c;
+    }
+    if (args.length === 2) {
+      const [a, b] = args;
+      return (x) => a * x + b;
+    }
+    if (args.length === 1) {
+      const [a] = args;
+      return () => a;
+    }
+  }
+  return null;
 }
 
 /**
@@ -105,8 +118,16 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const cache = new Map();
+
+  return (...args) => {
+    const key = func.toString();
+    if (!cache.has(key)) {
+      cache.set(key, func(...args));
+    }
+    return cache.get(key);
+  };
 }
 
 /**
